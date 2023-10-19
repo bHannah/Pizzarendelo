8e3764e0b8c92556c1b244838031326dd0b64e96
bHannah <borsikaaa1@gmail.com>
2023-10-19 21:23:42 +0200

    vegosszeg panel javitasa 2

--- Pizzarendelo/src/pizzarendelo/Pizzarendelo.java
+++ Pizzarendelo/src/pizzarendelo/Pizzarendelo.java
@@ -829,17 +829,17 @@ public class Pizzarendelo extends javax.swing.JFrame {
             if (fizmod == 1) {
                 title = "Gratulálunk!";
                 fizetes = "Készpénz";
-                uzenet = "A rendelésed: " + "\n" + darab +"db "+ fajta + "\n" + "Méret: " + meret + "\n" +  "Tészta:" + teszta + "\n" + "Extra:" + extra + "\n" +"A kiválasztott pizza ára: " + (osszeg * darab) + "\n" + "Fizetési mód: " + fizetes + "\n" + "Megjegyzésed: " + megjegyzes;
+                uzenet = "A rendelésed: " + "\n" + darab +"db "+ fajta + "\n" + "Méret: " + meret + "\n" +  "Tészta:" + teszta + "\n" + "Extra:" + extra + "\n" +"A kiválasztott pizza ára: " + (osszeg * darab) + " ft" + "\n" + "Fizetési mód: " + fizetes + "\n" + "Megjegyzésed: " + megjegyzes;
             }
             else if (fizmod == 2) {
                 title = "Gratulálunk!";
                 fizetes = "Bankkártya";
-                uzenet =  "A rendelésed: " + "\n" + darab +"db "+ fajta + "\n" + "Méret: " + meret + "\n" + "Tészta:" + teszta +  "\n" + "Extra:" + extra + "\n"+"A kiválasztott pizza ára: " + (osszeg * darab) + "\n" + "Fizetési mód: " + fizetes + "\n" + "Megjegyzésed: " + megjegyzes;
+                uzenet =  "A rendelésed: " + "\n" + darab +"db "+ fajta + "\n" + "Méret: " + meret + "\n" + "Tészta:" + teszta +  "\n" + "Extra:" + extra + "\n"+"A kiválasztott pizza ára: " + (osszeg * darab) +  " ft" +"\n" + "Fizetési mód: " + fizetes + "\n" + "Megjegyzésed: " + megjegyzes;
             }
             else if (fizmod == 3) {
                 title = "Gratulálunk!";
                 fizetes = "SZÉP-kártya";
-                uzenet =  "A rendelésed: " + "\n" + darab +"db "+ fajta + "\n" + "Méret: " + meret + "\n" +  "Tészta:" + teszta +  "\n" + "Extra:" + extra + "\n" +"A kiválasztott pizza ára: " + (osszeg * darab) + "\n" + "Fizetési mód: " + fizetes + "\n" + "Megjegyzésed: " + megjegyzes;
+                uzenet =  "A rendelésed: " + "\n" + darab +"db "+ fajta + "\n" + "Méret: " + meret + "\n" +  "Tészta:" + teszta +  "\n" + "Extra:" + extra + "\n" +"A kiválasztott pizza ára: " + (osszeg * darab) + " ft" + "\n" + "Fizetési mód: " + fizetes + "\n" + "Megjegyzésed: " + megjegyzes;
             }
         }
         
